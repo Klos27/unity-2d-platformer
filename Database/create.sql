d26abc526ff362d885022838c06ed0604209da05
misroka95 <misroka95@gmail.com>
2020-10-16 20:30:50 +0200

    Database and ERD added

---
+++ Database/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE GAME_USER (
+    id INT NOT NULL AUTO_INCREMENT,
+    login VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE USER_POINTS (
+    user_id INT NOT NULL,
+    world_id INT NOT NULL,
+    points INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES GAME_USER(id)
+);
