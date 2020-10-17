da9f6e03b043bbe3abb6ce78bd0b0cfb29ad0996
Jagoda <jagoda.palwal@gmail.com>
2020-10-17 16:47:30 +0200

    add left and right movement

--- Unity2DPlatformer/Assets/PlayerController.cs
+++ Unity2DPlatformer/Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    private int xVector = 5;
 
 
     // Start is called before the first frame update
@@ -17,9 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-
+            rb.velocity = new Vector2(-xVector, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(xVector, rb.velocity.y);
         }
     }
 }
