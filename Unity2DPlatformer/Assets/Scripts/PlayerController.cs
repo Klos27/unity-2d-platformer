f304999b4cb35f2313636bf487b3bdf1b3154b66
Klos27 <viperowski@gmail.com>
2020-11-06 23:41:03 +0100

    Add level timer

--- Unity2DPlatformer/Assets/Scripts/PlayerController.cs
+++ Unity2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     private Animator anim;
     private Collider2D coll;
@@ -20,8 +20,12 @@ public class PlayerController : MonoBehaviour
     private int coin20Points = 20;
     private int coin10Points = 10;
 
+    private const int maxLevelTimeInSeconds = 120;
+    private int levelTimeLeftInSeconds = maxLevelTimeInSeconds;
+
     [SerializeField] private int coinPoints = 0;
-    [SerializeField] private Text coinPointsText;
+    [SerializeField] private TMP_Text timerText = null;
+    [SerializeField] private TMP_Text pointsText = null;
 
     // Start is called before the first frame update
     void Start()
@@ -87,19 +91,37 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void UpdateTimerText()
+    {
+        float actualTime = Time.timeSinceLevelLoad;
+        levelTimeLeftInSeconds = maxLevelTimeInSeconds - (int)(actualTime);
+
+        if (levelTimeLeftInSeconds < 0)
+            levelTimeLeftInSeconds = 0;
+
+        int minutes = levelTimeLeftInSeconds / 60;
+        int seconds = levelTimeLeftInSeconds % 60;
+        timerText.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+
+    void UpdatePointsText()
+    {
+        pointsText.text = "Points: " + coinPoints.ToString("0000");
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Coin20P")
         {
             Destroy(collision.gameObject);
             coinPoints += coin20Points;
-            coinPointsText.text = coinPoints.ToString();
-        }
+            UpdatePointsText();
+        } 
         else if (collision.tag == "Coin10P")
         {
             Destroy(collision.gameObject);
             coinPoints += coin10Points;
-            coinPointsText.text = coinPoints.ToString();
+            UpdatePointsText();
         }
     }
 }
