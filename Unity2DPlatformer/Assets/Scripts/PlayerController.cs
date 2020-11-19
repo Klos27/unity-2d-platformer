e22ba5819e56e7eee78b48ffefffafcc49e23c3a
Verigoify <verigoify@gmail.com>
2020-11-19 21:03:25 +0100

    fix stuff in spikes

--- Unity2DPlatformer/Assets/Scripts/PlayerController.cs
+++ Unity2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     private enum State { idle, running, jumping, falling };
     private State state = State.idle;
     [SerializeField] private LayerMask ground = 0;
+    private Vector3 startingPos;
 
     private int xVector = 8;
     private int yVector = 60;
@@ -42,6 +43,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         coll = GetComponent<Collider2D>();
+        startingPos = GameObject.Find("Player").transform.position;
     }
 
     // Update is called once per frame
@@ -195,8 +197,6 @@ public class PlayerController : MonoBehaviour
             return 3;
         else
             return 1;
-
-        // TODO Fill according to design
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -277,6 +277,6 @@ public class PlayerController : MonoBehaviour
     void Die()
     {
         FindObjectOfType<AudioManager>().Play("GameOver");
-        rb.position = new Vector2(-5f, 6.11f);
+        rb.position = new Vector2(startingPos.x, startingPos.y);
     }
 }
